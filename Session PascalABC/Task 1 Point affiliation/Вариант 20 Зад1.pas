8fd96461a445fb43989a9a4ffc7ba0bb5ac08ab2
Pavel Lamzikov <Lamzikov@mail.ru>
2020-11-28 23:17:33 +0500

    Add files via upload

---
+++ Session PascalABC/Task 1 Point affiliation/Вариант 20 Зад1.pas
@@ -0,0 +1,11 @@
+﻿var x,y:integer;
+begin
+  writeln('Введите X точки M');
+  readln(x); 
+  writeln('Введите Y точки M');
+  readln(Y);
+  if (x<=4)and(y<=3*x)and (y>=0)then
+  writeln('Точка M принадлежит области')
+  else
+    writeln('Точка M не принадлежит области')
+end.
